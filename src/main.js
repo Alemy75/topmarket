94ed5f32ea8d7212023abb0f8071a47ee84946e5
Maxim Alekseev <max.alek@truelabel.io>
2025-05-29 09:49:01 +0300

    feat: sidebar

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
-import "./assets/style.css";
+import './assets/style.css';
+
+// Sidebar
+const sidebar = document.getElementById('sidebar');
+const openButton = document.getElementById('sidebar-open');
+const closeButton = document.getElementById('sidebar-close');
+
+openButton.addEventListener('click', () => {
+  sidebar.classList.remove('translate-x-[-100%]');
+});
+
+closeButton.addEventListener('click', () => {
+  sidebar.classList.add('translate-x-[-100%]');
+});
