f704c8815ebdaf9c4548bd2f46a7559f1c029dbf
Maxim Alekseev <max.alek@truelabel.io>
2025-08-08 09:04:31 +0300

    feat: cookie, policy

--- src/main.js
+++ src/main.js
@@ -20,3 +20,33 @@ links.forEach((link) => {
     sidebar.classList.add('translate-x-[-100%]');
   });
 });
+
+// form
+const checkbox = document.getElementById('agreeCheckbox');
+const submitBtn = document.getElementById('submitBtn');
+
+checkbox.addEventListener('change', function () {
+  submitBtn.disabled = !this.checked;
+  submitBtn.classList.toggle('opacity-50', !this.checked);
+  submitBtn.classList.toggle('cursor-not-allowed', !this.checked);
+  submitBtn.classList.toggle('cursor-pointer', this.checked);
+  submitBtn.classList.toggle('pointer-events-none', !this.checked);
+});
+
+// cookie
+const cookie = document.getElementById('cookie');
+const closeCookie = document.getElementById('cookie-close');
+const btnCookie = document.getElementById('cookie-btn');
+
+setTimeout(() => {
+  cookie.classList.add('duration-500');
+  cookie.classList.remove('translate-y-[150%]');
+}, 2000);
+
+closeCookie.addEventListener('click', () => {
+  cookie.classList.add('translate-y-[150%]');
+});
+
+btnCookie.addEventListener('click', () => {
+  cookie.classList.add('translate-y-[150%]');
+});
